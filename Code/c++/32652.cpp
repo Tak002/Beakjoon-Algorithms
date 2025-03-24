c2552e20ed32a2d46e497a0201e5bebe2b761052
Tak002 <andycho1120@naver.com>
2025-03-24 21:04:43 +0900

    32652 아카라카2

---
+++ Code/c++/32652.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int k; cin>>k;
+    cout<<"AKA";
+    for(int i=0; i<k; i++){cout<<"RAKA";}
+    return 0;
+}
